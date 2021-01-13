6d96b7391b5c45ebad2c8ea4b4e257aae379f00a
Reshin311 <311reshin@gmail.com>
2021-01-13 22:47:16 +0900

    第７回途中まで

---
+++ pentagonTableView21/View/TweetList/TweetListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetListTableViewCell.swift
+//  pentagonTableView21
+//
+//  Created by Shin on 2021/01/13.
+//
+
+import UIKit
+
+class TweetListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
